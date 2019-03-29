5db456ee01f0782212af728f4abe87403f6cf579
csm789 <sp.satyadeveloper@gmail.com>
2019-03-29 11:26:20 +0000

    Added line to Provider.tf

--- Create_sample_instance.tf
+++ Create_sample_instance.tf
@@ -12,21 +12,20 @@ resource "aws_instance" "Sample_instance" {
 }
 
 /*
-# Configure the AWS Provider
+{
+Below is a sample provider.tf file
+NOTE : You need Provider.tf to replace access_key and secret_key with generated values
+
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
+  access_key = "AGHKJHLKJHLH23"
+  secret_key = "DJLDDGGHH"
   region     = "us-east-1"
-  ====================================
+}
+  ==============================================================================
   These are the four ways to Password Credentials:
   1) Creating IAM user and password through provider.tf
   2) Creating IAM user and password through Environment variables
   3) Attach Admin roles to the user
   4) Storing it under ~/.aws/
-}
-
-# Create a web server
-resource "aws_instance" "web" {
-  # ...
-}
+================================================================================
 */
