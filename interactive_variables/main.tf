cc087d75aa156d29718b5a2535e92a24ef166649
csm789 <sp.satyadeveloper@gmail.com>
2019-04-01 22:51:14 +0100

    Setup to go with interactive_Variables Terraform

--- interactive_variables/main.tf
+++ interactive_variables/main.tf
@@ -1,1 +1,10 @@
 # Main
+
+resource "aws_instance" "Sample_instance" {
+  ami           = "ami-922914f7"
+  instance_type = "t2.micro"
+
+  tags {
+    Name = "demoinstance"
+  }
+}
--- interactive_variables/provider.tf
+++ interactive_variables/provider.tf
@@ -1,1 +1,6 @@
 # Provider
+provider "aws" {
+    access_key = "AKIAU4XACUQ73ITOE5WV"
+    secret_key = "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"
+    region     = "us-east-2"
+  }
